15efa5ef9b365d73883890da27c6873dd098e5a8
vvpvvp <alias8516@sina.com.cn>
2017-07-09 14:46:15 +0800

    新版

--- components/demos/dataplugins/autocomplete4.vue
+++ components/demos/dataplugins/autocomplete4.vue
@@ -10,6 +10,8 @@
 import jsonp from 'fetch-jsonp';
 
 const loadData = function (filter, callback) {
+  // this 为 option 配置
+  // this.orgId 使用传递的参数获取数据，例：搜索某公司下的员工
   jsonp(`https://suggest.taobao.com/sug?code=utf-8&q=${filter}`)
     .then(response => response.json())
     .then((d) => {
@@ -24,6 +26,7 @@ export default {
     return {
       value: '测试',
       param: {
+        orgId: 1, //自定义参数传递
         loadData,
         minWord: 1
       }
